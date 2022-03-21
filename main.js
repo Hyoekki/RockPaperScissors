f73e0341f67ba749ff55895c62f5443b32d03ee1
Hyoekki <janne.hyokki@gmail.com>
2022-03-21 16:10:35 +0200

    Added alert that tells who got 5 wins first

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
+[computerScore, playerScore] = [0, 0];
 const resultsDisplay = document.querySelector("#results");
 const selectionsDisplay = document.querySelector("#selections");
 const selections = ["rock", "paper", "scissors"];
+let finalWinner = document.getElementById("finalWinner");
 
 const handleClick = (e) => {
   const playerSelection = e.target.innerHTML;
   const computerSelection =
     selections[Math.floor(Math.random() * selections.length)];
-  playRound(playerSelection, computerSelection);
-  console.log(playerSelection, computerSelection);
+  resultsDisplay.innerHTML = playRound(playerSelection, computerSelection);
+  document.getElementById("playerScore").innerHTML = playerScore;
+  document.getElementById("computerScore").innerHTML = computerScore;
+  document.getElementById(
+    "computerPick"
+  ).innerHTML = `Computer chose: ${computerSelection}`;
+  let wins = checkWins();
+  if (wins >= 5) {
+    if (playerScore == 5) {
+      alert("Congratulations! You won 5 times!");
+      resetGame();
+    } else alert("Computer won 5 times, better luck next time!");
+    resetGame();
+  }
 };
+
+function resetGame() {
+  computerScore = 0;
+  playerScore = 0;
+}
 
 selections.forEach((selections) => {
   const button = document.createElement("button");
@@ -20,20 +39,25 @@ selections.forEach((selections) => {
 function playRound(playerSelection, computerSelection) {
   playerSelection = selections.indexOf(playerSelection);
   computerSelection = selections.indexOf(computerSelection);
-  console.log(playerSelection, computerSelection);
   if (playerSelection == computerSelection) {
-    resultsDisplay.innerHTML = "Tie";
+    return "Tie";
   }
   if (playerSelection == selections.length - 1 && computerSelection == 0) {
-    resultsDisplay.innerHTML = "Computer wins";
+    computerScore++;
+    return "Computer wins";
   }
   if (computerSelection == selections.length - 1 && playerSelection == 0) {
-    resultsDisplay.innerHTML = "Player wins";
+    playerScore++;
+    return "Player wins";
   }
   if (playerSelection > computerSelection) {
-    resultsDisplay.innerHTML = "Player wins";
+    playerScore++;
+    return "Player wins";
   } else {
-    resultsDisplay.innerHTML = "Computer wins";
+    computerScore++;
+    return "Computer wins";
   }
-  console.log(playerSelection, computerSelection);
+}
+function checkWins() {
+  return Math.max(computerScore, playerScore);
 }
